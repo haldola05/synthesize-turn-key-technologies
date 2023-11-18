723a1085f606601195204f8875033d7f9758a167
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-18 12:43:20 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+    const data = generateRandomData();
+    data.forEach(item => {
+    return data;
+
+    }
+    for (let i = 0; i < 10; i++) {
+    });
+}
+function main() {
+function generateRandomData() {def main():
+
+
+    for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+
+    return data
+import random
+    main()
+    data = generate_random_data()
