9d2f87ea18df717c0bd6b46e59b85682ad949a51
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-18 13:19:42 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,29 @@
 
+        print(f"Random Number: {item}")
+def main():
 
+if __name__ == "__main__":
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    main()
+
+    data = generate_random_data()
+def generate_random_data():
+    for item in data:    return data;
+    const data = generateRandomData();
         data.push(Math.floor(Math.random() * 100) + 1);
 }
-    const data = [];
-        console.log("Random Number: " + item);
-main();
-    const data = generateRandomData();
-    data.forEach(item => {
-    return data;
-
-    }
-    for (let i = 0; i < 10; i++) {
-    });
-}
 function main() {
-function generateRandomData() {def main():
 
+    });
+function generateRandomData() {
+        console.log("Random Number: " + item);
+    }
 
-    for item in data:
-def generate_random_data():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-
-    return data
-import random
-    main()
-    data = generate_random_data()
+}
+main();
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    const data = [];
